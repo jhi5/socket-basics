4998001a2fcfce2c2580791b2c5c9c93960f91a4
J. Munford <jaemunford@gmail.com>
2016-02-23 16:26:04 -0500

    added messaging

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,17 @@ socket.on("connect", function(){
 socket.on("message", function(message){
 	console.log("New message: " + message.text);
 });
+
+//message submisssion
+var $form = jQuery("#message-form");
+var message = $form.find('input[name=message]');
+
+$form.on("submit", function(event){
+	event.preventDefault();
+	socket.emit("message", {
+		text: message.val()
+	});
+
+message.val("");
+
+});
