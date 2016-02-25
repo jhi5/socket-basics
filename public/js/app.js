254524c965d913cf35e98c1418ee428b06b203cd
J. Munford <jaemunford@gmail.com>
2016-02-25 12:28:56 -0500

    added room functionality

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,17 @@ var socket = io();
 
 socket.on("connect", function(){
 	console.log("Connected to socket.io");
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on("message", function(message){
 	var momentTimestamp = moment.utc(message.timestamp);
 	var $message = jQuery('.messages');
-
+	var $room = jQuery('.room-title');
+	$room.text(room);
 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
 	$message.append('<p>' + message.text + '</p>');
 });
@@ -22,6 +27,7 @@ $form.on("submit", function(event){
 	event.preventDefault();
 	socket.emit("message", {
 		name: name,
+		room: room,
 		text: $message.val()
 	});
 
